f30151c51d8bc6cd46ea41c57acd91d53e21ad65
Deepak Guru <58070815+DeepakGuru1998@users.noreply.github.com>
2020-07-06 15:00:09 +0200

    Update harry.py

--- harry.py
+++ harry.py
@@ -1,50 +1,36 @@
 import numpy as np
 import cv2
-from PIL import Image
 
-video = cv2.VideoCapture('Learn Colors With Balls _ Colours For Kids And Children _ Learning & Education For Toddlers & Babies.mp4')
+video = cv2.VideoCapture(0)
+y=0
+while video.isOpened():
+    ret, test_image = video.read()
+    if y == 0:
+        ref_image=test_image
+        y=y+1   
+    test_image1 = cv2.cvtColor(test_image, cv2.COLOR_BGR2HSV)
+    lower_blue1 = np.array([110,45,45])
+    upper_blue1 = np.array([135,255,255])
+    lower_blue2 = np.array([40,25,40])
+    upper_blue2 = np.array([110,200,255])
+    mask_blue1 = cv2.inRange(test_image1, lower_blue1, upper_blue1)
+    mask_blue2 = cv2.inRange(test_image1, lower_blue2, upper_blue2)
+    mask_blue=mask_blue1+mask_blue2
+#    lower_red = np.array([0,130,0])
+#    upper_red = np.array([200,255,255])
+#    mask_red = cv2.inRange(test_image1, lower_red, upper_red)
 
-ret, test_image = video.read()
-ref_image = cv2.imread('shrimp.jpg')
-ref_image =cv2.resize(ref_image,(720,1280))
-while video.isOpened() and ret:
-    
-    #test_image = cv2.imread('red.jpg')
-    test_image = cv2.cvtColor(test_image, cv2.COLOR_BGR2HSV)
-    lower_blue = np.array([110,45,45])
-    upper_blue = np.array([135,255,255])
-    mask_blue = cv2.inRange(test_image, lower_blue, upper_blue)
-    
-    image = Image.fromarray(mask_blue)
-    ref_image= Image.fromarray(ref_image)
-    test_image1= Image.open("red.jpg")
-    image = image.convert("RGBA")
-    datas = image.getdata()
-    ref_datas = ref_image.getdata()
-    test_datas = test_image1.getdata()
-    newData = []
-    x=0
-    for item in datas:
-        if item[0] == 255 and item[1] == 255 and item[2] == 255:
-            newData.append((ref_datas[x]))
-        elif item[0] == 0 and item[1] == 0 and item[2] == 0:
-            newData.append((test_datas[x]))
-        else:
-            newData.append(item)
-        x=x+1
-    
-    image.putdata(newData)
-    #image.show()
-    open_cv_image = np.array(image) 
-    # Convert RGB to BGR 
-    open_cv_image = open_cv_image[:, :, ::-1].copy() 
-    cv2.imshow("frame",open_cv_image)
-    if cv2.waitKey(10) == 27:
+    mask_blue_not = cv2.bitwise_not(mask_blue)
+     
+    frame_1 = cv2.bitwise_and(ref_image,ref_image,mask=mask_blue)
+    frame_2 = cv2.bitwise_and(test_image,test_image,mask=mask_blue_not)
+    frame= cv2.addWeighted(frame_1,1,frame_2,1,0)
+    cv2.imshow("frame",frame)
+    if cv2.waitKey(1) == 27:
         break
 
 video.release()
 cv2.destroyAllWindows()
-    #cv2.waitKey()
 
 
 
