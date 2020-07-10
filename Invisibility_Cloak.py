f4cdeafce574b6cd8f3278f6a30c0131b7bee20d
Deepak Guru <58070815+DeepakGuru1998@users.noreply.github.com>
2020-07-10 12:24:07 +0200

    Update Invisibility_Cloak.py
    Used blue color as default hiding cloak color

--- Invisibility_Cloak.py
+++ Invisibility_Cloak.py
@@ -3,23 +3,24 @@ import cv2
 
 video = cv2.VideoCapture(0)
 y=0
+kernel = np.ones((5,5),np.uint8)
+
 while video.isOpened():
     ret, test_image = video.read()
     if y == 0:
         ref_image=test_image
         y=y+1   
+    
     test_image1 = cv2.cvtColor(test_image, cv2.COLOR_BGR2HSV)
-    lower_blue1 = np.array([110,45,45])
-    upper_blue1 = np.array([135,255,255])
+    lower_blue1 = np.array([90,45,160])
+    upper_blue1 = np.array([120,255,255])
+    mask_blue1 = cv2.inRange(test_image1, lower_blue1, upper_blue1)
     lower_blue2 = np.array([40,25,40])
     upper_blue2 = np.array([110,200,255])
-    mask_blue1 = cv2.inRange(test_image1, lower_blue1, upper_blue1)
     mask_blue2 = cv2.inRange(test_image1, lower_blue2, upper_blue2)
     mask_blue=mask_blue1+mask_blue2
-#    lower_red = np.array([0,130,0])
-#    upper_red = np.array([200,255,255])
-#    mask_red = cv2.inRange(test_image1, lower_red, upper_red)
 
+    cv2.morphologyEx(mask_blue, cv2.MORPH_CLOSE, kernel)
     mask_blue_not = cv2.bitwise_not(mask_blue)
      
     frame_1 = cv2.bitwise_and(ref_image,ref_image,mask=mask_blue)
